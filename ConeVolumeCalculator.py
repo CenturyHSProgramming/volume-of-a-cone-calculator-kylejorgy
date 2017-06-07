6b84c6d4aa1da2a80e441598046e18776c38f172
Jorgensen_Kyle <jorgk607@hsd.k12.or.us>
2017-06-07 09:54:43 -0700

    done

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -6,8 +6,11 @@
 
 # Define Function below
 # be sure to return an integer
-
-
+from math import pi
+def calculateConeVolume(r,h):
+    volume = pi*r**2*(h/3)
+    volume = round(volume,2)
+    return volume
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
